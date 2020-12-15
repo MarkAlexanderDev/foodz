b16dd79832538062c86f896c7827843805820abc
Guillaume Loiseleux <guillaume.loiseleux@epitech.eu>
2020-12-15 12:24:41 +0100

    + Made the content of this screen responsive. + Also brought the same changes to the previous onboarding screen (about food allergies). + Added the nbsp constant in the const file

--- lib/screens/onboarding/onboardingSteps/3-onboardingFavoriteFood.dart
+++ lib/screens/onboarding/onboardingSteps/3-onboardingFavoriteFood.dart
@@ -1,3 +1,4 @@
+import 'package:EasyGroceries/screens/consts.dart';
 import 'package:EasyGroceries/style/textStyle.dart';
 import 'package:EasyGroceries/widgets/selectable_tags.dart';
 import 'package:auto_size_text/auto_size_text.dart';
@@ -8,35 +9,37 @@ import 'package:lottie/lottie.dart';
 class OnboardingFavoriteFood extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         Lottie.asset('assets/lotties/food-prepared.json'),
-        AutoSizeText(
-          "What are your favorite types of food?",
-          style: textStyleH1,
-        ),
-        Expanded(
-          child: Container(
-            padding: EdgeInsets.only(left: 12.0, right: 12.0),
-            child: SelectableTags([
-              "Pizza",
-              "Halal",
-              "Japanese",
-              "Sushi",
-              "Indian",
-              "African",
-              "Thai",
-              "Italian",
-              "Chinese",
-              "Desserts",
-              "Healthy",
-              "French",
-              "American",
-              "Salad",
-              "Oriental",
-              "Pasta",
-            ]),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: AutoSizeText(
+            "What are your favorite types${nbsp}of${nbsp}food?",
+            style: textStyleH1,
+            textAlign: TextAlign.center,
           ),
+        ),
+        Container(
+          padding: EdgeInsets.all(24.0),
+          child: SelectableTags([
+            "Pizza",
+            "Halal",
+            "Japanese",
+            "Sushi",
+            "Indian",
+            "African",
+            "Thai",
+            "Italian",
+            "Chinese",
+            "Desserts",
+            "Healthy",
+            "French",
+            "American",
+            "Salad",
+            "Oriental",
+            "Pasta",
+          ]),
         ),
       ],
     );
